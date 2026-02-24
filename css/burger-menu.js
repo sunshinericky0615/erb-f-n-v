f70c1b071fd8e2fbe795c0f706e8bdff28d974b7
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-02-24 17:52:13 +0800

    update

---
+++ css/burger-menu.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-item");
+  toggleButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
