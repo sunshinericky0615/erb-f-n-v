ac2bda237ba737dc32641f6011f40dde8cf2193e
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-02-25 17:15:01 +0800

    update

---
+++ JS/1. dom into.js
@@ -0,0 +1,12 @@
+// console.log(window);
+// console.dir(window.document);
+// console.log(document.body);
+// console.log(document.links[0]);
+// insert element inside element
+// document.body.innerHTML = "<h1>hello from body</h1>";
+//  inspect text protion of html element
+// console.log(document.body.innerText);
+//  write something into document
+// document.write("Hello from JS");
+// document.getElementById("main").innerHTML = "<h1>hello from body</h1>";
+document.querySelector("#main h1").innerText = "Hello";
