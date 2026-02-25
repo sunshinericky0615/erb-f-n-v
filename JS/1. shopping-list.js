3a2bb9d69896e1f0b4c5b8c5a1ee0a56b64dc4e3
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-02-25 17:57:37 +0800

    update

---
+++ JS/1. shopping-list.js
@@ -0,0 +1,9 @@
+let output;
+output = document.all;
+output = document.all[11];
+output = document.all.length;
+output = document.documentElement;
+output = document.head;
+output = document.body;
+output = document.body.children;
+console.log(output);
