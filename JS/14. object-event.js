6512e778ff6735dee07a6f780e41a9a99a60be4a
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-03 21:35:55 +0800

    update

--- JS/14. object-event.js
+++ JS/14. object-event.js
@@ -1,5 +1,21 @@
-const logo = document.querySelector("img");
+const logo = document.querySelector("header");
 logo.addEventListener("click", (e) => {
   console.log(e);
   console.log(e.currentTarget);
+});
+function onClick(e) {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  e.target.style.backgroudColor = "red";
+  console.log(e.clientX);
+  console.log(e.clientY);
+  console.log(e.offsetX);
+  console.log(e.offsetY);
+}
+
+logo.addEventListener("click", onClick);
+
+document.querySelector("a").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("link is clicked");
 });
